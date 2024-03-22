f84b4c276e17b6b08f1cb2e6ebb07debdfc58d3f
Anatolii <toliankir@gmail.com>
2024-03-22 11:11:44 +0100

    Fix gramma

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,6 +6,10 @@
     <div class="flex justify-center items-center flex-col">
         <p class="font-bold text-blue-700 uppercase">Main test task 20240315</p>
     </div>
+
+    <div class=" text-center">
+        <a class="text-blue-700 underline hover:cursor-pointer" href="https://anatolii.vasyliev.name/">Anatolii Vasyliev</a>
+    </div>
 </template>
 
 <style scoped></style>

2deadb072da92440ddc2246ab120449f1e75705d
Anatolii <toliankir@gmail.com>
2024-03-22 10:29:18 +0100

    Fix get api url

--- src/helpers/get-apiurl.ts
+++ src/helpers/get-apiurl.ts
@@ -1,10 +1,22 @@
 export const getApiUrl = (path: string, opts?: {
     searchPramas?: URLSearchParams,
-    protocol?: "http" | "ws"
+    protocol?: "http" | "https" | "ws" | "wss"
 }): URL => {
     const apiUrl = new URL(import.meta.env.VITE_API_URL || (window as any).apiUrl);
     apiUrl.pathname = path;
-    apiUrl.protocol = opts?.protocol ? `${opts.protocol}:` : "http:";
+    switch (opts?.protocol) {
+        case "ws":
+        case "wss":
+            if (apiUrl.protocol === 'http:') {
+                apiUrl.protocol = 'ws:'
+            }
+            if (apiUrl.protocol === 'https:') {
+                apiUrl.protocol = 'wss:'
+            }
+            break;
+        default:
+            break;
+    }
 
     if (opts?.searchPramas) {
         for (const key of opts.searchPramas.keys()) {
